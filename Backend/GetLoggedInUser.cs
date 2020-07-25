0c60063eec3cf1f83f48dd5fda9ab21e7b8b1564
Nootn <me@nootn.com.au>
2020-07-25 13:18:24 +1000

    Starts building backend

---
+++ Backend/GetLoggedInUser.cs
@@ -0,0 +1,25 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace TrackerSafe.Backend
+{
+  public static class GetLoggedInUser
+  {
+    [FunctionName("GetLoggedInUser")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+        ILogger log)
+    {
+      log.LogInformation("Running!");
+
+      return new OkObjectResult("I don't work yet :P");
+    }
+  }
+}
