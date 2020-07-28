5bcf1ef2ac85abbfca10599d017763ab426df0c8
Nootn <me@nootn.com.au>
2020-07-28 22:34:02 +1000

    Gets workflow around installing the app there

--- App/AppConstants.cs
+++ App/AppConstants.cs
@@ -9,6 +9,7 @@ namespace TrackerSafe.App
     public const string PageUrlGetLocation = "/get-location";
     public const string PageUrlPushNotifications = "/push-notifications";
     public const string PageUrlPushNotificationsTest = "/push-notifications-test";
+    public const string PageUrlInstalledApp = "/installed-app";
     public const string InvalidSession = "/invalid-session";
     public const string UnexpectedError = "/unexptected-error";
   }
