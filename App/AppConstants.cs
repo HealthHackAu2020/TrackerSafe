cdc8361ed970e4dd50c2f42b4e64b47049bc89cc
Nootn <me@nootn.com.au>
2020-07-29 19:25:12 +1000

    Applied some stylz! and mock up of profile

--- App/AppConstants.cs
+++ App/AppConstants.cs
@@ -10,6 +10,7 @@ namespace TrackerSafe.App
     public const string PageUrlPushNotifications = "/push-notifications";
     public const string PageUrlPushNotificationsTest = "/push-notifications-test";
     public const string PageUrlInstalledApp = "/installed-app";
+    public const string PageUrlMyProfile = "/my-profile";
     public const string InvalidSession = "/invalid-session";
     public const string UnexpectedError = "/unexptected-error";
   }
