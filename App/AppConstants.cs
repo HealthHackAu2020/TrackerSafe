1b467a4e59572f6361fb8b95e55a76650447c82c
Nootn <me@nootn.com.au>
2020-08-01 11:48:52 +1000

    Links to logout

--- App/AppConstants.cs
+++ App/AppConstants.cs
@@ -14,6 +14,7 @@ namespace TrackerSafe.App
     public const string PageUrlInstalledApp = "/installed-app";
     public const string PageUrlMyProfile = "/my-profile";
     public const string PageUrlCheckIn = "/check-in";
+    public const string PageUrlRewards = "/rewards";
     public const string PageUrlReferralCode = "/referral-code";
     public const string InvalidSession = "/invalid-session";
     public const string UnexpectedError = "/unexptected-error";
